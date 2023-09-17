17ddd523197ae8645ce159fd4c75fc5ae865fc14
Andrew Chung <aandrewchung@gmail.com>
2023-09-17 17:16:43 -0400

    separated big ass fn into multi helpers

--- scripts/panelsfromstory.js
+++ scripts/panelsfromstory.js
@@ -2,42 +2,33 @@ const generateChatGPTResponse = require('./gptprompt.js');
 const extractTextChunks = require('./extracttext.js');
 
 
-async function extractPanels(story, rawCharacters) {
-  
-  // PARSE CHARACTERS INTO STRING DATA
-  const charactersPrompt = 
-    `You are a helpful bot that takes a paragraph describing characters and outputs organized data about each character. 
-     Identify the name of each character in the paragraph and their description.
-     Return the data in the form below
-     "Character Name: Description"
-     For example: Andrew is a college student.
-     "Andrew: college student"
-
-    The character paragraph is: ` + rawCharacters;
-
+async function parseCharacters(rawCharacters) {
+  const charactersPrompt = `You are a helpful bot that takes a paragraph describing characters and outputs organized data about each character. 
+    Identify the name of each character in the paragraph and their description.
+    Return the data in the form below "Character Name: Description"
+    For example: Andrew is a college student. "Andrew: college student"
+    
+    The character paragraph is: ${rawCharacters}`;
+    
   const parsedCharacters = await generateChatGPTResponse(charactersPrompt);
-  // console.log(parsedCharacters);
-  // console.log("\n");
 
-  // PARSE CHARACTERS STRING DATA INTO DICTIONARY
-  var charactersDict = {};
-  var lengthOfDescs = 0;
-
-  console.log(story);
-  console.log(rawCharacters);
-  var lines = parsedCharacters.split("\n");
-  for (var i = 0; i < lines.length; i++) {
-    var name_data = lines[i].split(": ");
+  const charactersDict = {};
+  let lengthOfDescs = 0;
+  
+  const lines = parsedCharacters.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    const name_data = lines[i].split(": ");
     charactersDict[name_data[0]] = name_data[1];
     lengthOfDescs += parseInt(name_data[1].length);
   }
-  console.log(charactersDict);
-  console.log("\n");
   
+  return { charactersDict, lengthOfDescs };
+}
 
+async function parseStory(story, charactersDict, lengthOfDescs) {
   // GENERATE 8 STORY BULLET POINTS
   const max_chars = 256 - lengthOfDescs - 75;
-  
+
   const storyPrompt = 
     `You are a helpful bot that takes a story and generates 8 key points. 
     Choose 8 points that would make a good story and be easily drawn for a comic book written for children.
@@ -49,14 +40,12 @@ async function extractPanels(story, rawCharacters) {
 
     Format example: 
     "1. The girl ran down the street
-     2. The boy ran up the street"
+    2. The boy ran up the street"
 
     The story is as follows: ` + story;
-  
+
 
   const rawDescription = await generateChatGPTResponse(storyPrompt);
-  // console.log(rawDescription);
-  // console.log("\n");
 
   // PARSE STORY INTO A STRING WITH CHARACTER DESCRIPTIONS
 
@@ -72,13 +61,13 @@ async function extractPanels(story, rawCharacters) {
     } 
     descriptionString += " "
   }
-  
-  console.log("descriptionString");
+
+  console.log("The descrpt string: ");
   console.log(descriptionString);
   console.log("\n");
 
+
   // PARSE STORY INTO A ARRAY WITH CHARACTER DESCRIPTIONS
-  
   var descriptionArray = []
   
   var lines = descriptionString.split("\n");
@@ -86,31 +75,28 @@ async function extractPanels(story, rawCharacters) {
     descriptionArray.push(lines[i]);
   }
 
-  console.log("descriptionArray");
-  console.log(descriptionArray);
-  console.log("\n");
+  return {descriptionString, descriptionArray}
+}
 
+async function parseDialogue(descriptionString) {
   // RUN THE DIALOGUE PROMPT
 
   const dialoguePrompt = 
   `You are a helpful robot that takes 8 bullet points telling a story and creates comic book style bullet points.
-   Each bullet point is a description of an image.
-   Write MAXIMUM five words to pair with each bullet point as if it was a comic book.
-   If the image is easily understood without words write "Dialogue: NULL".
-   Pretend to be speaking from the point of view of a character OR make a sound effect OR exclamation.
-   For example: "I found you!" or "Wow!" or "BANG!" or "CRASH" or "Where is he?"
-   Return the data in the following form:
-   "Image: value
+  Each bullet point is a description of an image.
+  Write MAXIMUM four words to pair with each bullet point as if it was a comic book.
+  If the image is easily understood without words write "Dialogue: NULL".
+  Pretend to be speaking from the point of view of a character OR make a sound effect OR exclamation.
+  For example: "I found you!" or "Wow!" or "BANG!" or "CRASH" or "Where is he?"
+  Return the data in the following form:
+  "Image: value
     Dialogue: value".
     Do not include any "" in the return value.
 
-   The bullet points are as follows` + descriptionString;
+  The bullet points are as follows` + descriptionString;
 
   const rawDialogue = await generateChatGPTResponse(dialoguePrompt);
-
-  // console.log("rawDialogue:")
-  // console.log(rawDialogue);
-  // console.log("\n")
+  console.log(rawDialogue);
 
   // PARSE THE DIALOGUE INTO AN ARRAY
 
@@ -125,10 +111,10 @@ async function extractPanels(story, rawCharacters) {
       dialogue = "";
       for (var j = 1; j < words.length; j++) {
         if (words[j] != "") {
-         dialogue += words[j] + " ";
-         if (words[j].length > longestLength) {
+        dialogue += words[j] + " ";
+        if (words[j].length > longestLength) {
           longestLength = words[j].length;
-         }
+        }
         }
 
         if (words[j] == "NULL") {
@@ -138,28 +124,32 @@ async function extractPanels(story, rawCharacters) {
       dialogueArray.push([dialogue, longestLength]);
     }
   }
+    return dialogueArray;
+}
 
+async function extractPanels(story, rawCharacters) {
+  const { charactersDict, lengthOfDescs } = await parseCharacters(rawCharacters);
+  console.log(charactersDict);
+  console.log("\n");
+  
+  const {descriptionString, descriptionArray} = await parseStory(story, charactersDict, lengthOfDescs);
+  console.log("The descrp arr: ");
+  console.log(descriptionArray);
+  console.log("\n");
+  
+  const dialogueArray = await parseDialogue(descriptionString);
   console.log("DIALOGUE");
   console.log(dialogueArray);
   console.log("\n");
 
-  // You can also extract text chunks here if needed
-  // const chunks = extractTextChunks(originalPoints);
-  //console.log(chunks);
-
-  // return chunks;
   return [descriptionArray, dialogueArray];
 }
 
 module.exports = { extractPanels };
 
 
-const story = `
-In the final moments of the championship match, with the score tied and tension in the air, Andrew, a steadfast member of the soccer team, seized the opportunity of a lifetime. As the clock ticked down, he dribbled past defenders with unwavering determination, the ball seemingly an extension of his being. With a swift, precise strike, he sent the ball soaring into the net, igniting a deafening roar from the crowd. Andrew's last-minute goal secured victory, etching his name in soccer lore and crowning his team as tournament champions.
+const story = "At a Pittsburgh Hackathon, Judge John walks around a gymnasium full of computers and people. He sees a group of four college students: Aleks, Andrew, Hugo, and Eric. They created incredible manga comic book project. Hugo and Aleks tell John a story about their project. John looks at the computer and smiles at the amazing comic book. He gives Eric a big high five. Andrew, Hugo, Aleks, and Eric win the hackathon and celebrate.";
 
-`;
+const chara = "John is a friendly man with gray hair and glasses. Aleks is a girl with short brown hair, Hugo is a tall boy with black curly hair, and Andrew is boy with short black hair, Eric is a boy in green hoodie.";
 
-const characters = "Andrew is a white haired male with blue eyes";
-
-
-// extractPanels(story, characters);
+extractPanels(story, chara);
